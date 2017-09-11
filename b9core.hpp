2aebb03988f278bd64afbb8bcbad778b545c3f86
Andrew Young <youngar17@gmail.com>
2017-09-11 14:33:02 -0400

    Fix bytecode numbers
     Bytecodes were hard coded to start at 0x0, which is used as a null
     terminator for bytecode streams.  In the future we will specify the
     size of a byte code stream, instead of relying on a null terminated
     stream.
    
    Signed-off-by: Andrew Young <youngar17@gmail.com>

--- b9core.hpp
+++ b9core.hpp
@@ -22,51 +22,51 @@ enum class ByteCode : ByteCodeSize {
     // Generic ByteCodes
 
     // Drop the top element of the stack
-    drop = 0x0,
+    drop = 0x1,
     // Duplicate the top element on the stack
-    duplicate = 0x1,
+    duplicate = 0x2,
     // Return from a function
-    functionReturn = 0x2,
+    functionReturn = 0x3,
     // Call a Base9 function
-    functionCall  = 0x3,
+    functionCall  = 0x4,
     // Call a native C function
-    primitiveCall = 0x4,
+    primitiveCall = 0x5,
     // Jump unconditionally by the offset
-    jmp = 0x5,
+    jmp = 0x6,
     // Push from a local variable
-    pushFromVar = 0x6,
+    pushFromVar = 0x7,
     // Push into a local variable
-    popIntoVar = 0x7,
+    popIntoVar = 0x8,
 
     // Integer bytecodes
 
     // Push a constant
-    intPushConstant = 0x8,
+    intPushConstant = 0x9,
     // Subtract two integers
-    intSub = 0x9,
+    intSub = 0xa,
     // Add two integers
-    intAdd = 0xa,
+    intAdd = 0xb,
     // Jump if two integers are equal
-    intJmpEq = 0xb,
+    intJmpEq = 0xc,
     // Jump if two integer are not equal
-    intJmpNeq = 0xc,
+    intJmpNeq = 0xd,
     // Jump if the first integer is greater than the second
-    intJmpGt = 0xd,
+    intJmpGt = 0xe,
     // Jump if the first integer is greater than or equal to the second
-    intJmpGe = 0xe,
+    intJmpGe = 0xf,
     // Jump if the first integer is less than to the second
-    intJmpLt = 0xf,
+    intJmpLt = 0x10,
     // Jump if the first integer is less than or equal to the second
-    intJmpLe = 0x10,
+    intJmpLe = 0x11,
 
     // String ByteCodes
 
     // Push a string from this module's constant pool
-    strConstant = 0x11,
+    strPushConstant = 0x12,
     // Jump if two strings are equal
-    strJmpEq = 0x12,
+    strJmpEq = 0x13,
     // Jump if two integer are not equal
-    strJmpNeq = 0x13,
+    strJmpNeq = 0x14,
 };
 
 class ByteCodes final {

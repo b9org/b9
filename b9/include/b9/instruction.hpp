2bd5f30aa1339ee0378fded3d1887fc151cc91d8
Robert Young <rwy0717@gmail.com>
2017-10-24 14:12:01 -0400

    Make Instruction a constexpr type
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- b9/include/b9/instruction.hpp
+++ b9/include/b9/instruction.hpp
@@ -19,54 +19,54 @@ using Parameter = std::int32_t;  // even though only 24 bits used
 /// ```
 class Instruction {
  public:
-  Instruction() noexcept : raw_{0} {}
+  constexpr Instruction() noexcept : raw_{0} {}
 
-  Instruction(RawInstruction raw) noexcept : raw_{raw} {}
+  constexpr Instruction(RawInstruction raw) noexcept : raw_{raw} {}
 
-  Instruction(ByteCode bc) noexcept
+  constexpr Instruction(ByteCode bc) noexcept
       : raw_{RawInstruction(bc) << BYTECODE_SHIFT} {}
 
-  Instruction(ByteCode bc, Parameter p) noexcept
+  constexpr Instruction(ByteCode bc, Parameter p) noexcept
       : raw_{(RawInstruction(bc) << BYTECODE_SHIFT) | (p & PARAMETER_MASK)} {}
 
-  Instruction &set(ByteCode bc, Parameter p) noexcept {
+  constexpr Instruction &set(ByteCode bc, Parameter p) noexcept {
     raw_ = (RawInstruction(bc) << BYTECODE_SHIFT) | (p & PARAMETER_MASK);
     return *this;
   }
 
   /// Encode the bytecode
-  Instruction &byteCode(ByteCode bc) noexcept {
+  constexpr Instruction &byteCode(ByteCode bc) noexcept {
     raw_ = RawByteCode(bc) << BYTECODE_SHIFT | parameter();
     return *this;
   }
 
   /// Decode the bytecode
-  ByteCode byteCode() const noexcept {
+  constexpr ByteCode byteCode() const noexcept {
     return static_cast<ByteCode>(raw_ >> BYTECODE_SHIFT);
   }
 
   /// Encode the parameter
-  Instruction &parameter(Parameter p) noexcept {
+  constexpr Instruction &parameter(Parameter p) noexcept {
     raw_ = (raw_ & BYTECODE_MASK) | (p & PARAMETER_MASK);
     return *this;
   }
 
   /// Decode the parameter
-  Parameter parameter() const noexcept {
+  constexpr Parameter parameter() const noexcept {
     auto param = static_cast<Parameter>(raw_ & PARAMETER_MASK);
     // Sign extend when top (24th) bit is 1
     if (param & 0x0080'0000) param |= 0xFF00'0000;
     return param;
   }
 
-  RawInstruction raw() const noexcept { return raw_; }
+  constexpr RawInstruction raw() const noexcept { return raw_; }
 
-  operator RawInstruction() const noexcept { return raw(); }
+  constexpr operator RawInstruction() const noexcept { return raw(); }
 
  private:
-  static const RawInstruction BYTECODE_SHIFT = 24;
-  static const RawInstruction PARAMETER_MASK = 0x00FF'FFFF;
-  static const RawInstruction BYTECODE_MASK = ~PARAMETER_MASK;
+  static constexpr const RawInstruction BYTECODE_SHIFT = 24;
+  static constexpr const RawInstruction PARAMETER_MASK = 0x00FF'FFFF;
+  static constexpr const RawInstruction BYTECODE_MASK = ~PARAMETER_MASK;
 
   RawInstruction raw_;
 };

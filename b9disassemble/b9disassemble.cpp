bb291f4b5ac329b8e33d2e89c9dbc3a1431b0f7c
Arianne <arianne.butler@ibm.com>
2017-11-28 13:14:40 -0500

    Add functions to parse section and function code
    Signed-off-by: Arianne Butler <arianne.butler@ibm.com>

--- b9disassemble/b9disassemble.cpp
+++ b9disassemble/b9disassemble.cpp
@@ -2,17 +2,19 @@
 #include <iterator>
 #include <string.h>
 
-// Parse uint32
-uint32_t uint32Parser(std::istream &in) {
-  uint32_t value;
-  in.read((char *)&value, sizeof(value));
-  std::cout << "The value is: " << value << std::endl;
-  
-  return 0;
+/* Parse uint32 */
+uint32_t uint32Parser(std::istream &in, std::uint32_t &out) {
+  in.read((char *)&out, sizeof(out));
+  std::cout << "The uint32_t value is: " << out << std::endl;
+  if (sizeof(out) != 4) {
+    std::cout << "Incorrect size of uint32_t" << std::endl;
+    return false;
+  } else {
+    return true;
+  }
 }
 
-
-// Read header "b9module" from module 
+/* Read header "b9module" from module */
 bool parseHeader(std::istream &in) {
   char buffer[8];
   in.read(buffer, 8);
@@ -21,30 +23,65 @@ bool parseHeader(std::istream &in) {
     return false;
   if (0 != strncmp("b9module", buffer, gcount))
     return false;
+  // TODO Set header in Module
   return true;
 }
 
-int parseSectionCode(std::istream &in) {
- return 0;
+/* Read Section Code  */
+bool parseSectionCode(std::istream &in) {
+ uint32_t sectionCode;
+ if (!uint32Parser(in, sectionCode)) {
+   return false;
+ }
+ if (sectionCode != 1) {
+   std::cout << "Incorrect section code" << std::endl;
+   return false;
+ }
+ std::cout << "Section Code: " << sectionCode << std::endl;
+ // TODO Set section code in Module
+ return true;
 }
 
+/* Read Function Count  */
+bool parseFunctionCount(std::istream &in) {
+  uint32_t functionCount;
+  if (!uint32Parser(in, functionCount)) {
+    return false; 
+  }
+  std::cout << "Function count: " << functionCount << std::endl;
+  // TODO Set function count in module
+  return true;
+}
 
+/* Disassemble Binary Module  */
 bool disassemble(std::istream &in, std::ostream &out) {
- parseHeader(in);
- uint32Parser(in);
- //parseSectionCode(in);
- // TODO call various parsing functions 
+ // Read header
+ if (parseHeader(in)) {
+   std::cout << "Success in parseHeader" << std::endl;
+ } else {
+   std::cout << "Failure in parseHeader" << std::endl;
+   return false;
+ }
+ // Read section code
+ if (parseSectionCode(in)) {
+   std::cout << "Success in parseSectionCode" << std::endl;
+ } else {
+   std::cout << "Failure in parseSectionCode" << std::endl;
+   return false;
+ }
+ // Read function count 
+ parseFunctionCount(in);
+
  return true;
 }
 
 int main (int argc, char** argv) {
   bool ok = disassemble(std::cin, std::cout);
   if (!ok) {
-    std::cout << "Fail" << std::endl;
+    std::cout << "Failure in disassemble" << std::endl;
     return 1;
-  }
-  else {
-    std::cout << "Success" << std::endl;
+  } else {
+    std::cout << "Success in disassemble" << std::endl;
     return 0;
   }
 }

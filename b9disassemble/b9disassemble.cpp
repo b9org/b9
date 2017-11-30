374ddde07c0347a680f36a1ff837f0742eeabf1a
Arianne <arianne.butler@ibm.com>
2017-11-30 15:22:56 -0500

    Change uint32 parser and add read bytecodes
    Change uint32 parser to generic number parser.
    Add function to read sequence of instructions and print the bytecodes.
    Add output parameters to all functions.
    Move print statements to disassemble function to ensure that the values
    are being set correctly.
    
    Signed-off-by: Arianne Butler <arianne.butler@ibm.com>

--- b9disassemble/b9disassemble.cpp
+++ b9disassemble/b9disassemble.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 #include <iterator>
+#include <memory>
+#include <vector>
 #include <string.h>
 
-/* Parse uint32 */
-uint32_t uint32Parser(std::istream &in, std::uint32_t &out) {
-  in.read((char *)&out, sizeof(out));
-  std::cout << "The uint32_t value is: " << out << std::endl;
-  if (sizeof(out) != 4) {
-    std::cout << "Incorrect size of uint32_t" << std::endl;
+#include <b9/instructions.hpp>
+
+using namespace b9;
+
+template <typename Number>
+bool parseNumber(std::istream &in, Number &out, long bytes = sizeof(Number)) {
+  long count = 0;
+  char* buffer = (char*)&out;
+  do {
+    in.read(&buffer[count], bytes);
+    count += in.gcount();
+  } while (count < bytes && in.good());
+  std::cout << "The value is: " << out << std::endl;
+  if (count != bytes) {
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
 
 /* Read header "b9module" from module */
-bool parseHeader(std::istream &in) {
-  char buffer[8];
+bool parseHeader(std::istream &in, char* buffer) {
   in.read(buffer, 8);
   auto gcount = in.gcount();
   if (gcount != 8)
@@ -28,80 +36,109 @@ bool parseHeader(std::istream &in) {
 }
 
 /* Read Section Code  */
-bool parseSectionCode(std::istream &in) {
- uint32_t sectionCode;
- if (!uint32Parser(in, sectionCode)) {
+bool parseSectionCode(std::istream &in, uint32_t &sectionCode) {
+ if (!parseNumber(in, sectionCode, sizeof(sectionCode))) {
    return false;
  }
  if (sectionCode != 1) {
    std::cout << "Incorrect section code" << std::endl;
    return false;
  }
- std::cout << "Section Code: " << sectionCode << std::endl;
  // TODO Set section code in Module
  return true;
 }
 
 /* Read Function Count  */
-bool parseFunctionCount(std::istream &in) {
-  uint32_t functionCount;
-  if (!uint32Parser(in, functionCount)) {
+bool parseFunctionCount(std::istream &in, uint32_t &functionCount) {
+  if (!parseNumber(in, functionCount, sizeof(functionCount))) {
     return false; 
   }
-  std::cout << "Function count: " << functionCount << std::endl;
   // TODO Set function count in module
   return true;
 }
 
-/* Read function information  */
-bool parseFunctionData(std::istream &in) {
-  uint32_t functionIndex;
-  uint32_t nargs;
-  uint32_t nregs;
-  if (!uint32Parser(in, functionIndex)) {
+/* Read function data  */
+bool parseFunctionData(std::istream &in, std::vector<uint32_t> &functionData) {
+  if (!parseNumber(in, functionData[0], sizeof(functionData[0]))) {
     return false;
   }
-  if (!uint32Parser(in, nargs)) {
+  if (!parseNumber(in, functionData[1], sizeof(functionData[1]))) {
     return false;
   }
-  if (!uint32Parser(in, nregs)) {
+  if (!parseNumber(in, functionData[2], sizeof(functionData[2]))) {
     return false;
   }
-  std::cout << "Function Index: " << functionIndex << std::endl;
-  std::cout << "Number Arguments: " << nargs << std::endl;
-  std::cout << "Number Registers: " << nregs << std::endl;
   // TODO Set function index, nargs, and nregs in Module
   return true;
+}
+
+/* Read the bytecodes */
+bool readInstructions(std::istream &in, std::shared_ptr<std::vector<Instruction>> &instructions) {
+  do {
+    RawInstruction instruction;
+    if (!parseNumber(in, instruction)) {
+      return false;
+    }
+    instructions->emplace_back(instruction);
+  } while (instructions->back() != END_SECTION);
+  // TODO Set bytecodes in Module 
+  return true;
+}
+
+/* Create Module  */
+void createModule(std::istream &in, std::ostream &out) {
+  
+
 }
 
 /* Disassemble Binary Module  */
 bool disassemble(std::istream &in, std::ostream &out) {
   // Read header
-  if (parseHeader(in)) {
-    std::cout << "Success in parseHeader" << std::endl;
+  char buffer[8];
+  if (parseHeader(in, buffer)) {
+    std::cout << "Success in parseHeader: " << buffer <<  std::endl;
   } else {
     std::cout << "Failure in parseHeader" << std::endl;
     return false;
   }
   // Read section code
-  if (parseSectionCode(in)) {
-    std::cout << "Success in parseSectionCode" << std::endl;
+  uint32_t sectionCode;
+  if (parseSectionCode(in, sectionCode)) {
+    std::cout << "Success in parseSectionCode: " << sectionCode << std::endl;
   } else {
     std::cout << "Failure in parseSectionCode" << std::endl;
     return false;
   }
   // Read function count 
-  if (parseFunctionCount(in)) {
-    std::cout << "Success in parseFunctionCount" << std::endl;
+  uint32_t functionCount;
+  if (parseFunctionCount(in, functionCount)) {
+    std::cout << "Success in parseFunctionCount: " << functionCount << std::endl;
   } else {
     std::cout << "Failure in parseFunctionCount" << std::endl;
     return false;
   }
   // Read function data
-  if (parseFunctionData(in)) {
+  std::vector<uint32_t> functionData(3);
+  if (parseFunctionData(in, functionData)) {
     std::cout << "Success in parseFunctionData" << std::endl;
+    std::cout << "Function index: " << functionData[0] << std::endl;
+    std::cout << "Number Arguments: " << functionData[1] << std::endl;
+    std::cout << "Number Registers: " << functionData[2] << std::endl;
   } else {
     std::cout << "Failure in parseFunctionData" << std::endl;
+  }
+  // Read bytecodes
+  auto instructions = std::make_shared<std::vector<Instruction>>();
+  if (readInstructions(in, instructions)) {
+    std::cout << "Success in readBytecodes" << std::endl;
+    std::cout << std::hex;
+    for (auto instruction : *instructions) {
+      std::cout << "Bytecode: " << instruction << std::endl;
+    }
+    std::cout << std::dec;
+  } else {
+    std::cout << "Failure in readBytecodes" << std::endl;
+    return false;
   }
   return true;
 }
@@ -116,4 +153,3 @@ int main (int argc, char** argv) {
     return 0;
   }
 }
-

f061cbb95c7710a5fd833c8f492301b656447ad0
Robert Young <rwy0717@gmail.com>
2018-02-21 11:11:19 -0500

    Let b9disassemble take an input file argument
    Signed-off-by: Robert Young <rwy0717@gmail.com>

--- b9disassemble/b9disassemble.cpp
+++ b9disassemble/b9disassemble.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 #include <b9/deserialize.hpp>
 #include <b9/module.hpp>
@@ -6,6 +7,19 @@
 using namespace b9;
 
 extern "C" int main(int argc, char** argv) {
-  auto module = deserialize(std::cin);
+  std::ifstream infile;
+  std::streambuf* inbuffer = nullptr;
+
+  if (argc == 1) {
+    inbuffer = std::cin.rdbuf();
+  }
+  else {
+    infile.open(argv[1], std::ios::in | std::ios::binary);
+    inbuffer = infile.rdbuf();
+  }
+
+  std::istream in(inbuffer);
+
+  auto module = deserialize(in);
   std::cout << *module;
 }
